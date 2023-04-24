79be62a00324f1ca32dfd7e6f92fee9f78bd7204
Xavier Loera Flores <x.loera.tech@gmail.com>
2023-04-24 01:23:20 -0700

    FEAT: Added Header & Footer elements to Layout.tsx

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,7 +1,16 @@
+import Footer from "./Footer";
+import Header from "./Header";
+
 type LayoutProps = {
   children: React.ReactNode;
 };
 
 export default function Layout({ children }: LayoutProps) {
-  return <div className="Page">{children}</div>;
+  return (
+    <div className="Page">
+      <Header />
+      {children}
+      <Footer />
+    </div>
+  );
 }
