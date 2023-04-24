ca785d95f625860bed0b88746d3899e634951965
Xavier Loera Flores <x.loera.tech@gmail.com>
2023-04-24 01:18:11 -0700

    FEAT: Added layout element to _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,14 @@
 import { type AppType } from "next/dist/shared/lib/utils";
 
 import "npm/styles/globals.css";
+import Layout from "npm/components/layout";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 };
 
 export default MyApp;
